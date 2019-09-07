70812c7ce9ef8685cf32a7246f0de71458701600
Naveen Sampath <ns7767@ad.rit.edu>
2019-09-07 12:11:54 -0400

    Added description and refactored the routes

--- app.js
+++ app.js
@@ -11,28 +11,30 @@ app.set('view engine', 'ejs');
 
 //SCHEMA SETUP - Adding new images.
 let collectionsSchema = new mongoose.Schema({
-    name:String,
-    image: String
+    name: String,
+    image: String,
+    description: String
 });
 
 let Collections = mongoose.model("Collections", collectionsSchema);
 
-Collections.create(
-    {
-        name: "Salmon Greek",
-        image:"https://cdn.pixabay.com/photo/2019/08/08/13/52/elephant-4393034__340.jpg"
-    },(err, collection) => {
-        if(err)
-        {
-            console.log(err);
-        }
-        else
-        {
-            console.log("Newly created collections");
-            console.log(collection);
-        }
-    }
-);
+// Collections.create(
+//     {
+//         name: "Men",
+//         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTJhE-tk8T6oOONZDh0sHA3v6Uov7SaZyC1Pe5ODci6kTOuMx6Pw",
+//         description: "This Zip-Up Hoodie with adjustable drawstring lets you lock in the warmth around your head, Sleeve Cuffs and Waistband help keep you warm, Kanga Pockets keep your hands warm and the inner pocket let you store valuables in this Hooded Sweatshirt"
+//     },(err, collection) => {
+//         if(err)
+//         {
+//             console.log(err);
+//         }
+//         else
+//         {
+//             console.log("Newly created collections");
+//             console.log(collection);
+//         }
+//     }
+// );
 
 //Mark: This was initial howe page landing setup.
 
@@ -41,34 +43,70 @@ app.get("/", (req, res) => {
    res.render("landing");
 });*/
 
-let collections = [
+/*let collections = [
     {name: "Men", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoecTctlgaOBnK6RjU0WaMQLrGqC6HzAqMZjcBvkut97HYt3r1ZwKK4t9h"},
     {name: "Women ", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzZhQrC9BsiitwWhhZmuJItSjT7_nI1_NCncds7nxA0waaAFw5"},
     {name: "Kids", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTJhE-tk8T6oOONZDh0sHA3v6Uov7SaZyC1Pe5ODci6kTOuMx6Pw"},
 ];
+*/
 
-
-app.get("/", (req, res) => {
-   res.render("collections", {collections: collections});
-});
-
+/*app.get("/", (req, res) => {
+   res.render("/", {collections: collections});
+});*/
+//Index - Show all the collections.
 app.get("/collections", (req, res) => {
-    res.render("collections", {collections:collections});
+
+    Collections.find((err, allCollections) => {
+        if(err)
+        {
+            console.log("Something went wrong");
+        }
+        else
+        {
+            res.render("index", {collections:allCollections});
+        }
+    });
 });
 
+//Create - add new collections to the database.
 app.post("/collections",(req, res) => {
     let name = req.body.name;
     let image = req.body.image;
-    let newCollection = {name:name, image:image};
-    collections.push(newCollection);
+    let description = req.body.description;
+    let newCollection = {name:name, image:image, description:description};
 
-    res.redirect("/collections");
+    //Create a new collection and save to DB
+    Collections.create(newCollection, (err, newlyCreated) => {
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.redirect("/collections");
+            console.log(newlyCreated);
+        }
+    });
 });
 
+
+//New - show form to create the collections.
 app.get("/collections/new", (req, res) => {
-   res.render("new.ejs");
+   res.render("new");
 });
 
+
+//Show the description of the new collections.
+app.get("/collections/:id",(req, res) => {
+    Collections.findById(req.params.id,(err, showCollection) => {
+       if(err)
+       {
+           console.log(err);
+       }
+       else
+       {
+           res.render("show", {collection: showCollection});
+       }
+    });
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("JustCotton server is started");
